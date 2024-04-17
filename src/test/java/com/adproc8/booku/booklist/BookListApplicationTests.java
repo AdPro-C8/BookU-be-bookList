4012567dfa1c8193475ee122dccc5ab3b302e7f1
adipppp <adhif2004@gmail.com>
2024-04-17 09:56:05 +0700

    Initial commit

---
+++ src/test/java/com/adproc8/booku/booklist/BookListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adproc8.booku.booklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
