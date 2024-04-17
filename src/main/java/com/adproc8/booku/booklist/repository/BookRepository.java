5f0e803caf0de6b1f627e987ae9694a0bba018ac
adipppp <adhif2004@gmail.com>
2024-04-17 20:16:34 +0700

    Add BookRepository

---
+++ src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
@@ -0,0 +1,19 @@
+package com.adproc8.booku.booklist.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.adproc8.booku.booklist.model.Book;
+import com.adproc8.booku.booklist.model.BookView;
+
+public interface BookRepository extends JpaRepository<Book, UUID> {
+    <S extends Book> S save(S book);
+    List<BookView> findBy();
+    List<BookView> findByTitle(String title);
+    List<BookView> findByAuthor(String author);
+    List<BookView> findByOrderByPublishDateAsc();
+    List<BookView> findByOrderByPublishDateDesc();
+    void deleteById(UUID bookId);
+}
