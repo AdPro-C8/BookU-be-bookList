7bde459f04047ce7455389fa10b4f7900ff7c226
adipppp <adhif2004@gmail.com>
2024-04-28 23:55:33 +0700

    Update BookRepository queries to return Book instance instead of BookView

--- src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
+++ src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
@@ -6,12 +6,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.adproc8.booku.booklist.model.Book;
-import com.adproc8.booku.booklist.model.BookView;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
-    List<BookView> findBy();
-    List<BookView> findByTitle(String title);
-    List<BookView> findByAuthor(String author);
-    List<BookView> findByOrderByPublishDateAsc();
-    List<BookView> findByOrderByPublishDateDesc();
+    List<Book> findByTitle(String title);
+    List<Book> findByAuthor(String author);
+    List<Book> findByOrderByPublishDateAsc();
+    List<Book> findByOrderByPublishDateDesc();
 }
