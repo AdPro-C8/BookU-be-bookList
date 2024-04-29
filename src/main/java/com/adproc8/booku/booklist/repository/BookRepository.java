3a3ad573f06d3be1a752e3a27fcb83a31ddd5795
adipppp <adhif2004@gmail.com>
2024-04-29 12:08:54 +0700

    [RED] Update BookRepository to support queries needed for BookService

--- src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
+++ src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
@@ -3,13 +3,16 @@ package com.adproc8.booku.booklist.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.adproc8.booku.booklist.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
     List<Book> findByTitle(String title);
+    List<Book> findByTitle(String title, Sort sort);
     List<Book> findByAuthor(String author);
-    List<Book> findByOrderByPublishDateAsc();
-    List<Book> findByOrderByPublishDateDesc();
+    List<Book> findByAuthor(String author, Sort sort);
+    List<Book> findByTitleAndAuthor(String title, String author);
+    List<Book> findByTitleAndAuthor(String title, String author, Sort sort);
 }
