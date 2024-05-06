b7b6b97c57bfcbdc20a15aa7ab4e1251ce49ef42
adipppp <adhif2004@gmail.com>
2024-05-06 15:55:48 +0700

    [RED] Add the needed queries to BookRepository

--- src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
+++ src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
@@ -4,15 +4,33 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.adproc8.booku.booklist.model.Book;
 
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
+    List<Book> findAll(Specification<Book> spec);
+    List<Book> findAll(Specification<Book> spec, Sort sort);
     List<Book> findByTitle(String title);
     List<Book> findByTitle(String title, Sort sort);
     List<Book> findByAuthor(String author);
     List<Book> findByAuthor(String author, Sort sort);
     List<Book> findByTitleAndAuthor(String title, String author);
     List<Book> findByTitleAndAuthor(String title, String author, Sort sort);
+
+    class BookSpecifications {
+        public static Specification<Book> authorIs(String author) {
+            return (root, query, builder) -> {
+                return builder.equal(root.get("author"), author);
+            };
+        }
+
+        public static Specification<Book> titleIs(String title) {
+            return (root, query, builder) -> {
+                return builder.equal(root.get("title"), title);
+            };
+        }
+    }
 }
