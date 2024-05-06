dcb4a856b7db14c99c43e8396f6f3b24b9c559f9
adipppp <adhif2004@gmail.com>
2024-05-06 16:13:40 +0700

    [GREEN] Implement BookController

--- src/main/java/com/adproc8/booku/booklist/controller/BookController.java
+++ src/main/java/com/adproc8/booku/booklist/controller/BookController.java
@@ -4,19 +4,25 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
 import com.adproc8.booku.booklist.model.Book;
 import com.adproc8.booku.booklist.service.BookService;
 
+import static com.adproc8.booku.booklist.repository.BookRepository.BookSpecifications.*;
+
 @RestController
 @RequestMapping("/book")
 class BookController {
 
-    private BookService bookService;
+    private final BookService bookService;
 
     @Autowired
     BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
     @GetMapping
@@ -24,6 +30,24 @@ class BookController {
         @RequestParam Optional<String> author, @RequestParam Optional<String> title,
         @RequestParam Optional<String> sortBy, @RequestParam Optional<String> sortOrder)
     {
-        return null;
+        Specification<Book> bookSpec = Specification.where(null);
+
+        if (author.isPresent()) {   
+            bookSpec = bookSpec.and(authorIs(author.get()));
+        }
+        if (title.isPresent()) {
+            bookSpec = bookSpec.and(titleIs(title.get()));
+        }
+
+        List<Book> bookList;
+
+        if (sortBy.isPresent() && sortOrder.isPresent()) {
+            Sort sort = Sort.by(Direction.fromString(sortOrder.get()), sortBy.get());
+            bookList = bookService.findAll(bookSpec, sort);
+        } else {
+            bookList = bookService.findAll(bookSpec);
+        }
+
+        return bookList;
     }
 }
