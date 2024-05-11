7705eed1cdb2889faa4725a4fc9ebfea36ec70e2
adipppp <adhif2004@gmail.com>
2024-05-11 10:51:47 +0700

    [CHORE] Add JWT and custom configuration properties

---
+++ src/main/java/com/adproc8/booku/booklist/config/SecurityConfiguration.java
@@ -0,0 +1,65 @@
+package com.adproc8.booku.booklist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    private static final String BOOK_PATTERN = "/book/**";
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(customizer -> customizer
+                        .requestMatchers(HttpMethod.GET, BOOK_PATTERN)
+                        .permitAll()
+                        .requestMatchers(HttpMethod.POST, BOOK_PATTERN)
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, BOOK_PATTERN)
+                        .hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, BOOK_PATTERN)
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
---
+++ src/main/java/com/adproc8/booku/booklist/model/User.java
@@ -0,0 +1,49 @@
+package com.adproc8.booku.booklist.model;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User implements UserDetails {
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    private Collection<? extends GrantedAuthority> authorities;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
