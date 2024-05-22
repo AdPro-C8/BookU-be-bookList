f85e7182047ec073efe2df355c5d194b6f5b557f
adipppp <adhif2004@gmail.com>
2024-05-22 18:31:24 +0700

    [REFACTOR] Remove unused method definition in BookRepository

--- src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
+++ src/main/java/com/adproc8/booku/booklist/repository/BookRepository.java
@@ -1,9 +1,7 @@
 package com.adproc8.booku.booklist.repository;
 
-import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -11,9 +9,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import com.adproc8.booku.booklist.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
-    List<Book> findAll(Specification<Book> spec);
-    List<Book> findAll(Specification<Book> spec, Sort sort);
-
     class BookSpecifications {
         public static Specification<Book> authorIs(String author) {
             return (root, query, builder) -> {
